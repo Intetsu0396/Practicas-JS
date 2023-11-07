a08f173f47a8dfe7c2fd28629d50762fbd362191
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2023-11-07 16:00:18 -0500

    Repositando la carpeta del estudio de JS
    Esta es la creacion de la carpeta para las practicas del curso de JS presentado por manos de 4Geeks

---
+++ Practica con JS/conditional.js
@@ -0,0 +1,34 @@
+console.log("Hola esto es un condicional JS para practicar");
+
+/*Los conicionales nos permite crear funciones
+ o ejecuciones para pragmentos del codigo */
+
+// primer  condicional es if 
+// segundo condicional es if & else
+//multi contidional es if & else if ... & else
+
+
+const edadDeEntrada = 18;
+
+const edadUsurario = 15;
+
+// {}= representan en este caso una linea de codigo
+if(edadUsurario>=edadDeEntrada){
+    //todo lo que este dentro de las llaves pertenece al "if"
+    console.log("Puedes entrar, tienes mas de 18")
+}
+else if(edadUsurario<=12){
+    console.log("Estamos llamando a la policia")
+}
+else{
+    /*dentro de las laves escribimos 
+    la accion para esta condicion */
+    console.log("Eres muy joven, mejor vete pa' tu casa pana")
+}
+
+//bucle / ciclos / loops
+// for: su erstructura es la siguiente
+// for(inicializacion; condicion; operacion)
+
+for(let)
+
---
+++ Practica con JS/index.js
@@ -0,0 +1,27 @@
+let a = 5 - 3;
+let modulo = 5%3;
+
+console.log(a);
+console.log(modulo)
+
+
+// operadores
+// > mayor que 
+// > menor que 
+// >= mayor o igual que 
+// <= menor o igual que 
+// = asigna valores
+// != diferente de ...
+// == compara valores 
+// === estricamente igual 
+// !== estrictamente diferente
+// ++ incremento en 1
+// -- decremento en 1 
+// "!nom_variable" niega el valor de la variable
+// && (and) aplica condiciones de la tabla de verdad
+// || (or) aplica  condicion de la talbe de verdad
+
+console.log(8 > 9);
+console.log(9>8);
+console.log(9>=8)
+
